20ca5ea39efb4b32f218c49bef51640479ef9fd2
suetak <akasirotonbo08451@gmail.com>
2023-09-25 22:28:10 +0900

    JWT を保持する構造体の宣言

--- connect-go-example/auth/jwt.go
+++ connect-go-example/auth/jwt.go
@@ -1,6 +1,9 @@
 package auth
 
-import _ "embed"
+import (
+	_ "embed"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+)
 
 // os.ReadFile を使うとファイルも適切なpath に置く必要があるため、embed を使って実行バイナリにファイルを埋め込む
 
@@ -9,3 +12,10 @@ var rawPrivateKey []byte
 
 //go:embed cert/public.pem
 var rawPublicKey []byte
+
+type JWTer struct {
+	PrivateKey, PublicKey jwk.Key
+	Store                 Store
+	//Clocker               clock.Clocker
+
+}
